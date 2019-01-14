20cfdcfd735ca841fc3aaec64fa182dad04baf44
natekamp <natekamp01@gmail.com>
2019-01-13 21:28:22 -0700

    Initial Commit

---
+++ app/src/main/java/natekamp/ideas/MainActivity.java
@@ -0,0 +1,58 @@
+package natekamp.ideas;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+    }
+
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_home:
+                Toast.makeText(this, "Home Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_friends:
+                Toast.makeText(this, "Friends Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_messages:
+                Toast.makeText(this, "Messages Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout Selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
