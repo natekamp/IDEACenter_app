1744e96479f726c962a786866647a87e55325f84
natekamp <natekamp01@gmail.com>
2019-04-16 11:46:52 -0600

    Started work on post editor activity

---
+++ app/src/main/java/natekamp/ideas/PostEditorActivity.java
@@ -0,0 +1,29 @@
+package natekamp.ideas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class PostEditorActivity extends AppCompatActivity
+{
+    private Button updateButton, deleteButton;
+    private EditText titleText, descriptionText;
+    private ImageButton attachmentButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_editor);
+
+    //Buttons and EditTexts
+        updateButton = (Button) findViewById(R.id.post_editor_update);
+        deleteButton = (Button) findViewById(R.id.post_editor_delete);
+        titleText = (EditText) findViewById(R.id.post_editor_title);
+        descriptionText = (EditText) findViewById(R.id.post_editor_description);
+        attachmentButton = (ImageButton) findViewById(R.id.post_editor_attachment);
+    }
+}
