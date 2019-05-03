2a2f954cbb518ca374c0aa13162bd7fb61e1bc0c
natekamp <natekamp01@gmail.com>
2019-05-03 11:42:20 -0600

    Working on video player activity.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Mobile app for the Loveland High School IDEA Center.
 
 Progress (vid:min:sec) = 28:00:00
+
+This is the first app I've ever made, so sorry if anything you see is horrendous. :)
--- app/src/main/java/natekamp/ideas/VideoActivity.java
+++ app/src/main/java/natekamp/ideas/VideoActivity.java
@@ -1,24 +1,35 @@
 package natekamp.ideas;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
-import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
-import static android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class VideoActivity extends AppCompatActivity
 {
     //extras
     String videoURL;
 
-    //views
-    private VideoView videoView;
+    //ExoPlayer
+    private SimpleExoPlayer player;
+    private long playbackPosition;
+    private int currentWindow;
+    private boolean playWhenReady = true;
 
-    //controller
-    private MediaController mediaController;
+    //views
+    private PlayerView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,14 +42,75 @@ public class VideoActivity extends AppCompatActivity
 
         //views
         videoView = findViewById(R.id.video_player);
-        videoView.setVideoPath(videoURL);
+    }
 
-        //controller
-        mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
 
-        videoView.setMediaController(mediaController);
-        videoView.requestFocus();
-        videoView.start();
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void initializePlayer() {
+        if (player == null) {
+            player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
+                    new DefaultTrackSelector(), new DefaultLoadControl());
+            videoView.setPlayer(player);
+            player.setPlayWhenReady(playWhenReady);
+            player.seekTo(currentWindow, playbackPosition);
+        }
+        MediaSource mediaSource = buildMediaSource(Uri.parse(videoURL));
+        player.prepare(mediaSource, true, false);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource.Factory(new DefaultHttpDataSourceFactory("exoplayer-codelab"))
+                .createMediaSource(uri);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        videoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
